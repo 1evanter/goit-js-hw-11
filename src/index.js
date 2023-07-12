2ebe0fe58533f45e0dd968884ce01845e76735c4
1evanter <rita.oleksenkoo@gmail.com>
2023-07-12 21:58:05 +0200

    render images

--- src/index.js
+++ src/index.js
@@ -4,26 +4,53 @@ import ImagesApiService from "./js/pixabay-api";
 
 const API_KEY = '38212223-f32e704a5bd5b7c02deacefa3';
 
-const formEl = document.querySelector('.search-form');
-const loadMoreBtn = document.querySelector('.load-more');
+const refs = {
+    formEl: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    imageListEl: document.querySelector('.image-list')
+};
+
 const imagesApiService = new ImagesApiService();
 
-
-formEl.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', onLoadMore)
+refs.formEl.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(evt) {
     evt.preventDefault();
 
     imagesApiService.query = evt.target.elements.searchQuery.value;
     imagesApiService.resetPage();
-    imagesApiService.fetchImages();
+    imagesApiService.fetchImages().then(addImageCard);
 }
 
 function onLoadMore() {
-     imagesApiService.fetchImages();
+     imagesApiService.fetchImages().then(addImageCard);
 }
 
+
+function addImageCard(hits) {
+ const galleryElements = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+     return   `<li class="image-item">
+     <div class="image-item-wrap">
+        <img
+          class="image"
+          src="${webformatURL}"
+          data-source="${largeImageURL}"
+          alt="${tags}"
+         
+        />
+        <div class="image-info">
+        <p class="image-likes">Likes: ${likes}</p>
+        <p class="image-views">Views: ${views}</p>
+        <p class="image-comments">Comments: ${comments}</p>
+        <p class="image-downloads">Downloads: ${downloads}</p>
+        </div>
+        </div>
+      </li>`
+ }).join('');
+    
+   return refs.imageListEl.insertAdjacentHTML('beforeend', galleryElements);
+}
 
 
 
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -13,7 +13,11 @@ export default class ImagesApiService{
 
         const url = `https://pixabay.com/api/?key=38212223-f32e704a5bd5b7c02deacefa3&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 
-        fetch(url).then(res => res.json()).then(this.incrementPage()); 
+        return fetch(url).then(res => res.json()).then(data => {
+            this.incrementPage();
+         
+            return data.hits;
+        });    
     };
 
     incrementPage() {
